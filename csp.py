eba7c523f65d48b9abd4b0f596317e42e22e0cdf
magixus <magixusneo@live.fr>
2018-12-12 19:32:26 +0100

    add random mp creation

--- csp.py
+++ csp.py
@@ -1,5 +1,9 @@
+#!/usr/bin/python3
+
 import numpy as np
 import random 
+import sys
+import re
 
 class X(object):
 	"""docstring for X"""
@@ -28,7 +32,6 @@ class C(object):
 						self.Mp[x][y] = np.identity(p)
 					else:
 						self.Mp[x][y] = np.ones((p,p))
-			return self.Mp
 			
 		else : 
 			"""
@@ -36,20 +39,34 @@ class C(object):
 			"""
 			for x in range(n):
 				for y in range(n):
-					
 
+					if x==y : 
+						for i in range(p):
+
+							# matrix p*p that had random 0.1 in diag
+							self.Mp[x][y][i][i] = random.randint(2) 
+
+					elif y > x:
+						rm = np.random.randint(2, size=(p, p))
+						rm_t = np.transpose(rm)
+						self.Mp[x][y] = rm # put a random matrix on mp(x,y)
+						self.Mp[y][x] = rm_t	#put it transpose on mp(y,x)
+					
 
 		
 
 
 class CSP(object):
 	"""docstring for CSP"""
-	def __init__(self, mp):
+	def __init__(self,x,d,c):
 		super(CSP, self).__init__()
-		self.mp = mp
+		self.x = x
+		self.d = d
+		self.c = c
 	
-	def REVISE_pc() :
-		pass
+	def REVISE_pc(self,Xi,Xk,Xj):
+		#temp = self.c.Mp[Xi][Xj] and 
+		pass 
 
 	def PC2() :
 		pass
@@ -57,7 +74,21 @@ class CSP(object):
 	def look_ahead() :
 		pass
 
+if len(sys.argv) > 2:
+	xi = sys.argv[1]
+	di = sys.argv[2]
+	p = r"\d+"
+	xi_rang = [int(x) for x in re.findall(p,xi)]
+	di_rang = [int(d) for d in re.findall(p,di)]
 
-test_mat = C(10,12)
 
-test_mat.Mp
+	print(xi_rang, "    ",di_rang,"  ")
+
+xi = X(xi_rang)
+di = D(di_rang)
+
+print(xi.Xi)
+print(di.Di)
+test_mat = C(xi,di)
+
+print(test_mat.Mp)
--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 import numpy as np
+import sys
+
+
 
 # test matrix creation 
+"""
 def matrix1 (n,p):
 	Mp = np.zeros((n,n,p,p),int)
 	for x in range(n):
@@ -15,5 +19,14 @@ csp = matrix1(5,10)
 print(csp[1])
 print(csp[1][1])
 print(csp[2][3])
+"""
 
-
+# test argument variable "amazing!"
+"""
+if len(sys.argv) > 2:
+	msg = sys.argv[1]
+	name = sys.argv[2]
+	print(msg, "    ",name)
+else :
+	print("hey what are you doing ?? !! stop being an idot")
+"""
