major fix on pc2 and look-ahead

basicFunctions.py
@@ -1,19 +1,14 @@
 import numpy as np
 from sty import fg, bg, ef, rs, Rule
 
-def intersection_m(m1, m2):
-	return  m1*m2
-
-def product_m(m1,m2):
-	k = np.dot(m1,m2)
-	for i in range(len(k[0])) :
-		for j in range(len(k[0])) :
-			if k[i,j] >1 : k[i,j] =1
-	return k 
 
 def printcolor(ob):
 	print(bg(np.random.randint(255)) + str(ob) + bg.rs)
 
 
-def printmatrix(m):
-	printcolor(m)
+
+def printt(m):
+	for i in range(len(m)):
+		for j in range(len(m)):
+			print("Mp[",i,',',j,"]")
+			print (m[i,j])

contrainte.py
@@ -16,15 +16,9 @@ class C(object):
 		def setconstraint(self,contraint):
 			self.constraint_xi_xj = contraint
 
-	def __mul__(self, other):
-		len_mat = len(self.constraint_xi_xj)
-		res = np.dot(self.constraint_xi_xj, other.constraint_xi_xj)
-		print(res.shape)
-		for i in range(res.shape[0]) :
-			for j in range(res.shape[1]) :
-				if res[i,j] > 1 : res[i,j] = 1
-		return C(res)
-
+	def is_empty(self):
+		if np.sum(self.constraint_xi_xj) == 0 : return True
+		else : return  False
 
 	def transpose(self):
 		p = len(self.constraint_xi_xj)
@@ -46,3 +40,11 @@ class C(object):
 				if self.constraint_xi_xj[i,j] != other.constraint_xi_xj[i,j]:
 					return False
 		return True
+
+	def __mul__(self, other):
+		len_mat = len(self.constraint_xi_xj)
+		res = np.dot(self.constraint_xi_xj, other.constraint_xi_xj)
+		for i in range(res.shape[0]) :
+			for j in range(res.shape[1]) :
+				if res[i,j] > 1 : res[i,j] = 1
+		return C(res)

csp.py
@@ -18,11 +18,13 @@ class CSP(object):
   """docstring for CSP"""
 
   def __init__(self, x, d,c):
-    super(CSP, self).__init__()
     self.x = X(x)  # number of variables
     self.d = D(d, x)  # range of domain definition
-    self.Mp = self.generateRandomConstraint(self.getQ(c))
     self.domainXi = [set([p for p in range(d)]) for y in range(x)]
+    if type(c) is 'int':
+      self.Mp = self.generateRandomConstraint(self.getQ(c))
+    else :
+      self.Mp = c
 
 
   def generateRandomConstraint(self, constraint):
@@ -54,32 +56,34 @@ class CSP(object):
       mat[xi, xj] = unchaged
       mat[xj, xi] = mat_t  # put transpose on mp(xj,xi)
 
-  def updatedomain(self):
+  def updatedomain(self,mat,domaine):
     for i in range(self.x.Xi) :
-      c = self.Mp[i,i]
+      c = mat[i,i]
       for l in range(len(c)):
-        if c[l,l] == 0 : self.domainXi[i].remove(l)
+        if c[l,l] == 0 :
+          domaine.exists[i][l] = 0
+          self.domainXi[i].remove(l)
 
-  def consistance(self, mat):
-    taille = len(mat)
-    for i in range(taille):
-      for j in range(i + 1, taille - 1):
+
+  def is_consistant(self,mat):
+    for i in range(self.x.Xi):
+      for j in range(self.x.Xi):
         # if any matrix is empty then false
         if np.sum(mat[i, j]) == 0: return False
     return True
 
-  def PC2(self, mat, Q):
+  def PC2(self,domaine, mat,Q):
 
     while Q:  # while Q still has elements on it
       pair = Q.pop()
-      i = pair[0];
-      j = pair[1]
+      print(pair)
+      i,j = pair[0],pair[1]
       for k in range(self.x.Xi):
         if (not (k == i) and not (k == j)):
           # green
           temp = C(mat[i, k]) & (C(mat[i, j]) * (C(mat[j, j]) * C(mat[j, k])))
           if not (C(temp) == C(mat[i, k])):
-            mat[i, k] = temp;
+            mat[i, k] = temp
             mat[k, i] = C(temp).transpose()
             if i < k:
               Q.add((i, k))
@@ -88,58 +92,52 @@ class CSP(object):
           # blue
           temp = C(mat[k, j]) & (C(mat[k, i]) * (C(mat[i, i]) * C(mat[i, j])))
           if not (C(temp) == C(mat[k, j])):
-            mat[k, j] = temp;
+            mat[k, j] = temp
             mat[j, k] = C(temp).transpose()
             if k < j:
               Q.add((k, j))
             else:
               Q.add((j, k))
-    return mat
+    #BF.printt(mat)
+    self.updatedomain(mat,domaine)
 
   ##except Exception as e:
   ##	return "faill"
 
-  def look_ahead_v1(self, m, Q):  # perform random var's initiation
-    # (x,d) = (variables, domains)
-    self.PC2(m, Q)
-    if not self.consistance(m): print("inconsistant"); return False
-    if self.x.isinstanciate():
+  def look_ahead(self, Domaine, m, Q):  # perform random var's initiation
+    QQ = Q
+    self.PC2(Domaine,m,QQ)
+    print("passage ici--------")
+    if not self.is_consistant(m): print("inconsistant"); return False
+    if self.x.is_instanciate():
       print("tout est instantié")
       return True
     else:
       # pic random var to istanciate
-      print(["vide" if not (e) else e for e in self.x.instanciation])
       xi = self.x.picRandomUninstanciateVar()
+      print(["vide" if not (e) else e for e in self.x.instanciation])
 
-      for vi in self.domainXi[xi]:
-        self.updatedomain(m, xi)
+      print(self.domainXi[xi])
+
+      for vi in Domaine.Di[xi]:
+        self.domainXi[xi]={vi}
+        dom = Domaine
+        dom.Di[xi] = {vi}
+        self.x.instanciation[xi] = {vi}
         m2 = m
-        self.x.instanciation[xi] = {[vi]}
-
-        # changement de domaine
-        self.updateconstraint(m2, xi, vi)
-        if self.look_ahead_v1(m2, Q): return True
-
+        BF.printt(m)
+        self.updateConstrait(dom,m2)
+        if self.look_ahead(dom, m2, Q): return True
       return False
 
-  def look_ahead_v2(self, m, Q):
-    # (x,d) = (variables, domains)
-    m = self.PC2(m, Q)
-    if not self.consistance(m): print("inconsistant"); return False
-    if self.x.isinstanciate():
-      print("tout est instantié")
-      return True
-    else:
-      # pic heuristic variable
-      xi = self.d.picHeuristicVar(self.x)
-      for vi in self.domainXi[xi]:
-        self.updatedomain(m, xi)
-        m2 = m
-        self.x.instanciation[xi] = {[vi]}
-
-        # changement de domaine
-        self.updateconstraint(m2, xi, vi)
-        if self.look_ahead_v1(m2, Q): return True
-
-      return False
-
+  def updateConstrait(self, dom, mat):
+    for i in range(self.x.Xi):
+      for j in range(i+1,self.x.Xi):
+        domC = dom.getConstraintFromDomain(i,j)
+        """
+        print("----")
+        print(domC)
+        """
+        mat[i,j] = mat[i,j] & domC
+        domC = np.transpose(domC)
+        mat[j,i] = mat[j,i] & domC

queens/reines.py
@@ -7,12 +7,11 @@ import random, sys, re
 class reines(object):
 	"""docstring for reines"""
 	def __init__(self, number):
-		super(reines, self).__init__()
-		self.number = number
+		self.mp = self.generateNQueenProblem(number)
 
 
-	def constraint(self):
-		p = self.number
+	def constraint(self,number):
+		p = number
 		Mp = np.ones((p,p,p,p),int)
 		for x in range(p):
 			for y in range(p):
@@ -33,14 +32,17 @@ class reines(object):
 
 		return Mp
 
-	def generateNQueenProblem(self):
-		n = self.number
-		matrix = self.constraint()
+	def generateNQueenProblem(self,number):
+		matrix = self.constraint(number)
 		return matrix
-		for i in range(n):
-			for j in range(n):
-				print("Mp[",i,',',j,"]")
-				print (matrix[i,j])
+
+	def getq(self, number):
+		Q = set()  # get all constraint between variables
+		for i in range(number):
+			for j in range(number):
+				if j > i:
+					if not (self.mp[i, j] == np.ones((number, number), int)).all():
+						Q.add((i, j))
+		return Q
 
 
-#generateNQueenProblem(int(sys.argv[1]))